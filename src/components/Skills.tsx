29c85d1776d3a6614017e40a55085370d47dbc5c
Javier Flores <jffc.dev@gmail.com>
2024-02-27 01:11:20 -0500

    feat: skills component added

---
+++ src/components/Skills.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+
+interface SkillProps {
+  name: string
+  x: string
+  y: string
+}
+
+const Skill = ({name, x, y}: SkillProps) => {
+  return(
+    <motion.div className="flex items-center justify-center rounded-full 
+                    fond-semibold
+                    bg-dark text-light py-3 px-6 shadow-dark absolute"
+        whileHover={{scale: 1.05}}
+        initial={{x: 0, y: 0}}
+        whileInView={{x: x, y: y}}
+        transition={{duration: 1.0}}
+        viewport={{once: true}}
+    >
+        {name}
+    </motion.div>
+  )
+}
+
+const Skills = () => {
+  return (
+    <>
+        <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
+        <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
+
+        <motion.div className="flex items-center justify-center rounded-full 
+                          fond-semibold
+                          bg-dark text-light p-8 shadow-dark absolute"
+              whileHover={{scale: 1.05}}
+          >
+              Web
+          </motion.div>
+
+          <Skill name='HTML' x='-20vw' y='2vw'></Skill>
+          <Skill name='CSS' x='-5vw' y='-10vw'></Skill>
+          <Skill name='Javascript' x='20vw' y='6vw'></Skill>
+          <Skill name='.NET' x='0vw' y='12vw'></Skill>
+          <Skill name='Python' x='-20vw' y='-15vw'></Skill>
+          <Skill name='SQL' x='15vw' y='-8vw'></Skill>
+          <Skill name='React' x='16vw' y='-20vw'></Skill>
+          <Skill name='C#' x='-10vw' y='22vw'></Skill>
+          <Skill name='Tailwind' x='18vw' y='18vw'></Skill>
+        </div>
+    </>
+  )
+}
+
+export default Skills
